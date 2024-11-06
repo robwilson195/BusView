4df4cc36507b0e15da519e64e885652959c2cf48
Rob Wilson <rob.wilson195@gmail.com>
2024-11-06 10:17:45 +0000

    Initial Commit

---
+++ BusView/BusViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusViewApp.swift
+//  BusView
+//
+//  Created by Catherine Megregian on 06/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct BusViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
